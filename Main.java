c29ffc0825b5632be00c1eb2f4c811fdec942324
Vsevolod21 <kononenko.vsevolod@gmail.com>
2022-03-21 12:41:12 +0300

    Second commit

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package task132;
 
 import java.io.*;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Main {
@@ -20,6 +21,13 @@ public class Main {
         saveGame("Games\\savegames\\save3.dat", gameProgress[2]);
 
         zipFiles(archiveName, dirName);
+
+        openZip(archiveName, dirName);
+
+        System.out.println(openProgress("Games\\savegames\\save1.dat"));
+        System.out.println(openProgress("Games\\savegames\\save2.dat"));
+        System.out.println(openProgress("Games\\savegames\\save3.dat"));
+
     }
 
     public static void saveGame(String path, GameProgress gp) {
@@ -35,7 +43,6 @@ public class Main {
         File zipDir = new File(zippedDir);
         String[] fileList = zipDir.list();
         ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(arhName));
-
         for (String value : fileList) {
             File f = new File(value);
             String filePath = f.getPath();
@@ -51,14 +58,47 @@ public class Main {
             }
         }
         zos.close();
-
         for (String s : fileList) {
             if (new File(zippedDir + "\\" + s).delete()) {
                 System.out.println("Файл " + s + " добавлен в архив " + arhName);
                 System.out.println("Файл " + s + " удален из папки: " + zippedDir);
             }
         }
-
     }
 
+    public static void openZip(String arhName, String dirName) {
+        try (ZipInputStream zin = new ZipInputStream(new FileInputStream(arhName))) {
+            ZipEntry entry;
+            String name;
+            long size;
+            while ((entry = zin.getNextEntry()) != null) {
+                name = entry.getName(); // получим название файла
+                size = entry.getSize(); // получим его размер в байтах
+                System.out.printf("File name: %s \t File size: %d \n", name, size);
+                // распаковка
+                FileOutputStream fout = new FileOutputStream(dirName + "\\" + name);
+                for (int c = zin.read(); c != -1; c = zin.read()) {
+                    fout.write(c);
+                }
+                fout.flush();
+                zin.closeEntry();
+                fout.close();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static GameProgress openProgress(String path) {
+        GameProgress gameProgress = null;
+        // откроем входной поток для чтения файла
+        try (FileInputStream fis = new FileInputStream(path);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            // десериализуем объект и скастим его в класс
+            gameProgress = (GameProgress) ois.readObject();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return gameProgress;
+    }
 }
